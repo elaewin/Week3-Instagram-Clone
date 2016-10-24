71eaa0368ef90ea017d8a3519833b86cf0b71233
Erica Winberry <elaewin@gmail.com>
2016-10-24 11:55:29 -0700

    Adding UIAlertController action sheet.

---
+++ InstagramClone/InstagramClone/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HomeViewController.swift
+//  InstagramClone
+//
+//  Created by Erica Winberry on 10/24/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presentImagePicker(sourceType: .photoLibrary)
+
+        // Do any additional setup after loading the view.
+    }
+
+    func presentImagePicker(sourceType: UIImagePickerControllerSourceType) {
+        
+        self.imagePicker.delegate = self
+        self.imagePicker.sourceType = sourceType
+        
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func presentActionSheet() {
+        let actionSheet = UIAlertController(title: "Source", message: "Please select source type!", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            self.presentImagePicker(sourceType: .camera)
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .destructive) { (action) in
+            self.presentImagePicker(sourceType: .photoLibrary)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            cameraAction.isEnabled = false
+        }
+        
+        actionSheet.addAction(cameraAction)
+        actionSheet.addAction(photoLibraryAction)
+        actionSheet.addAction(cancelAction)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+
+}
+
+extension HomeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+}
+
