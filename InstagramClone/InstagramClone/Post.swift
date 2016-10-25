6e6571993b6169bdb85bbda1ab3b2ffe43ba6c82
Erica Winberry <elaewin@gmail.com>
2016-10-25 09:56:28 -0700

    add post object.

---
+++ InstagramClone/InstagramClone/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  InstagramClone
+//
+//  Created by Erica Winberry on 10/25/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+import UIKit
+
+class Post {
+    
+    let image: UIImage
+    
+    init(image: UIImage) {
+        self.image = image
+    }
+    
+}
